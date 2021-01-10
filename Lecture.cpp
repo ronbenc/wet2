5f5ce514d47959b58a64f8e41dcb3ff009752eef
Ron Benchetrit <ronbenc@campus.technion.ac.il>
2021-01-10 14:59:36 +0200

    Update Lecture

--- Lecture.cpp
+++ Lecture.cpp
@@ -24,28 +24,17 @@ bool operator<(Lecture a, Lecture b)
 
 bool operator>(Lecture a, Lecture b)
 {
-    return !(a < b);
+    return !(a <= b);
 }
 
 bool operator<=(Lecture a, Lecture b)
 {
-    if(a.view_time != b.view_time)
-    {
-        return (a.view_time <= b.view_time);
-    }
-    else if(a.courseID != b.courseID)
-    {
-        return (a.courseID <= b.courseID);
-    }
-    else
-    {
-        return (a.classID <= b.classID);
-    }
+    return (a < b) || (a == b);
 }
 
 bool operator>=(Lecture a, Lecture b)
 {
-    return !(a <= b);
+    return !(a < b);
 }
 
 bool operator==(Lecture a, Lecture b)
